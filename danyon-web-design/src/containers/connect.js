6a86ecb53bba6cdad7cf2a101bd19d663c1ea3a7
danyonsatterlee <danyon.satterlee@gmail.com>
2017-07-17 17:55:15 -0600

    more animations

--- danyon-web-design/src/containers/connect.js
+++ danyon-web-design/src/containers/connect.js
@@ -1,34 +1,50 @@
 import React from "react";
 import ConntactForm from '../components/contact-form.js'
+import WOW from 'wowjs';
 let FontAwesome = require('react-fontawesome');
+
 class Contact extends React.Component {
+    componentDidMount() {
+        new WOW
+            .WOW()
+            .init();
+    }
     render() {
         return (
-            <div className="spacer">
+            <div className="spacer wow fadeIn" data-wow-offset="70"  data-wow-iteration="1">
                 <div className="row" id="connection">
 
-                    <div className="col-md-3 col-md-offset-2 col-sm-12 col-sm-offset-0">
+                    <div className="col-md-3 col-md-offset-2 col-sm-12 col-sm-offset-0 ">
                         <h1 className="text-center gray">let's connect</h1>
-                          <ConntactForm />
-                 
-                   
-                          
-                        </div>
-                      
-             
+                        <ConntactForm/>
+
+                    </div>
+
                     <div className="col-md-2 col-md-offset-1 col-sm-12 col-sm-offset-0 side">
-                      
+
                         <h1 className="gray name-header pull-right ">about</h1>
-                        </div>
-                        <div className="col-md-3 col-md-offset-0 col-sm-12 col-sm-offset-0">
-                              <p className="gray space aboutp">
-                           arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
+                    </div>
+                    <div className="col-md-3 col-md-offset-0 col-sm-12 col-sm-offset-0">
+                        <p className="gray space aboutp ">
+                            arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam
+                            dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus
+                            elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
+                            porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus
+                            in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius
+                            laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.
+                            Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas
+                            tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet
+                            adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar,
+                            hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae
+                            sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget
+                            eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec
+                            sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit
+                            cursus nunc,
                         </p>
                     </div>
                 </div >
-                 </div>
-                
-               
+            </div>
+
         );
     }
 }
--- danyon-web-design/src/containers/explore.js
+++ danyon-web-design/src/containers/explore.js
@@ -13,24 +13,29 @@ class Explore extends React.Component{
 
                 </div>
                 </div>
-                
+
             <div  className="row">
                 <div className="col-md-3 col-md-offset-1 col-sm-12 col-sm-offset-0 no-marg">
-                     <a href="#explore"><div className="explore1 wow fadeInDown" data-wow-offset="70" data-wow-delay=".25s" data-wow-iteration="1"></div></a>
-                     <a href="#explore"><div className="explore2 wow fadeInDown" data-wow-offset="70" data-wow-delay="1.25s" data-wow-iteration="1"></div></a>
+                     <div class="wrapImage">
+                     <a href="#explore"><div className="explore1 wow fadeIn" data-wow-offset="70" data-wow-delay=".25s" data-wow-iteration="1"></div></a>
+                       <div class="middle">
+    <div class="text">Moss Models</div>
+                     <a href="#explore"><div className="explore2 wow fadeIn" data-wow-offset="70" data-wow-delay="1.25s" data-wow-iteration="1"></div></a>
                     
                 </div>
+                </div>
+                </div>
                  <div className="col-md-4 col-sm-12 col-sm-offset-0 no-marg">
-                     <a href="#explore"><div className="explore4 wow fadeInDown" data-wow-offset="70" data-wow-delay=".75s" data-wow-iteration="1"></div></a>
-                     <a href="#explore"><div className="explore7 wow fadeInDown" data-wow-offset="70" data-wow-delay=".5s" data-wow-iteration="1"></div></a>
-                     <a href="#explore"><div className="explore3 wow fadeInDown" data-wow-offset="70" data-wow-delay="1.75s" data-wow-iteration="1"></div></a>
+                     <a href="#explore"><div className="explore4 wow fadeIn" data-wow-offset="70" data-wow-delay=".75s" data-wow-iteration="1"></div></a>
+                     <a href="#explore"><div className="explore7 wow fadeIn" data-wow-offset="70" data-wow-delay=".5s" data-wow-iteration="1"></div></a>
+                     <a href="#explore"><div className="explore3 wow fadeIn" data-wow-offset="70" data-wow-delay="1.75s" data-wow-iteration="1"></div></a>
                     
                 </div>
                      <div className="col-md-3 col-sm-12 col-sm-offset-0 no-marg">
                    
-                     <a href="#explore"><div className="explore6 wow fadeInDown" data-wow-offset="70" data-wow-delay="1.25s" data-wow-iteration="1"></div></a>
-                      <a href="#explore"><div className="explore4 wow fadeInDown" data-wow-offset="70" data-wow-delay="2s" data-wow-iteration="1"></div></a>
-                     <a href="#explore"><div className="explore5 wow fadeInDown" data-wow-offset="70" data-wow-delay="2.25s" data-wow-iteration="1"></div></a>
+                     <a href="#explore"><div className="explore6 wow fadeIn" data-wow-offset="70" data-wow-delay="1.25s" data-wow-iteration="1"></div></a>
+                      <a href="#explore"><div className="explore4 wow fadeIn" data-wow-offset="70" data-wow-delay="2s" data-wow-iteration="1"></div></a>
+                     <a href="#explore"><div className="explore5 wow fadeIn" data-wow-offset="70" data-wow-delay="2.25s" data-wow-iteration="1"></div></a>
                     
                 </div>
             </div>
