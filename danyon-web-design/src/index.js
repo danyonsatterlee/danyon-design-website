e6e3dfe9b666d83fe4ae871effe6eb1d58a42be0
danyonsatterlee <danyon.satterlee@gmail.com>
2017-09-12 08:40:44 -0600

    tracker number

--- danyon-web-design/src/index.js
+++ danyon-web-design/src/index.js
@@ -3,26 +3,27 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import './index.css';
 import Home from './containers/home.js'
-
+import createHistory from 'history/createBrowserHistory'
 import AllExplore from './containers/allexplore.js';
-
+import withTracker from './containers/tracker.js';
 import Explore from './containers/explore.js';
 let ReactGA = require('react-ga');
 
 ReactGA.initialize('UA-72855452-1');
 
-function logPageView() {
-  ReactGA.set({ page: window.location.pathname + window.location.search });
-  ReactGA.pageview(window.location.pathname + window.location.search);
-}
+const history = createHistory()
+history.listen((location, action) => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
 
 class App extends React.Component{
     render(){
         return(
 
 
-    <BrowserRouter onUpdate={logPageView}>
-        <Switch>
+    <BrowserRouter history={history} >
+        <Switch >
           <Route exact path="/" component={Home}/>
           <Route exact path="/allexplore" component={AllExplore} />
   
